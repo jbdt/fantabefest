7e1ae66143584c333ae89ef0193146ca2ce172b1
Javier Blanco <javierblancodetorres@gmail.com>
2024-05-29 16:01:26 +0700

    first commit

---
+++ assets/contact-eef99630f4397521ebd08584fb2f59e954a13e2ac3a81439ebcb8bbc83430610.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "come on, you have a name, don't you?",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                subject: {
+                    required: "Venga, de algo querrás hablar, ¿no?",
+                    minlength: "¡Al menos pon cuatro letras!"
+                },
+                number: {
+                    required: "come on, you have a number, don't you?",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "No email, no mensaje"
+                },
+                message: {
+                    required: "Uhm... entiendo. No en serio, cuéntanos bien",
+                    minlength: "¿Eso es todo en serio? Expláyate un poco más 😉"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"GET",
+                    data: $(form).serialize(),
+                    url:"/contact",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+                        $('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+                      $('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+});
